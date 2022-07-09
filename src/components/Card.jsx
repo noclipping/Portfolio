7ed5cd9293c6ea31230ebcb02dd12879ed8271c1
aresous <aresous@gmail.com>
2022-07-09 03:44:37 -0400

    props

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,23 +2,25 @@ import React from "react";
 import "../Card.css";
 import { BsGithub } from "react-icons/bs";
 import { BsFillPlayFill } from "react-icons/bs";
-export default function Card() {
+export default function Card({ project }) {
+  console.log(project);
   return (
-    <div className="card_container">
+    <div className={`card_container ${project?.background}`}>
       <div className="card_header">
         <span>
-          <BsGithub />
+          <a href={`${project?.repo}`} target="_blank">
+            <BsGithub />
+          </a>
         </span>
-        <h1>InstaClone</h1>
+        <h1>{project?.title}</h1>
         <span>
-          <BsFillPlayFill />
+          <a href={`${project?.demo}`} target="_blank">
+            <BsFillPlayFill />
+          </a>
         </span>
       </div>
       <div className="gif_container">
-        <img
-          className="gif"
-          src="https://media2.giphy.com/media/CJl7OCbeJuZfIGSjuE/giphy.gif"
-        />
+        <img className="gif" src={`${project?.gif}`} />
       </div>
     </div>
   );
