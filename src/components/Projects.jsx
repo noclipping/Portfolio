fbd2778e38c15f5cce4a13b277223356c6822879
aresous <aresous@gmail.com>
2022-07-09 03:44:46 -0400

    props

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,14 +2,21 @@ import React from "react";
 import "../ProjectPage.css";
 import Card from "./Card";
 export default function Projects() {
+  const instagram = {
+    title: "InstaClone",
+    background: "instaclone",
+    repo: "https://github.com/noclipping/instagramClone",
+    demo: "https://instagram-clone-pig9cvrvm-noclipping.vercel.app/",
+    gif: "https://media2.giphy.com/media/CJl7OCbeJuZfIGSjuE/giphy.gif",
+  };
   return (
     <section id="projects">
       <div className="cards_container">
-        <Card />
+        <Card project={instagram} />
 
-        <Card />
+        <Card project={instagram} />
 
-        <Card />
+        <Card project={instagram} />
       </div>
     </section>
   );
