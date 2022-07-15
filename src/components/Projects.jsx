3286e42b910c1dbe04c099edd42599f30c150054
aresous <aresous@gmail.com>
2022-07-14 20:46:49 -0400

    Social Humans Styling

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -9,14 +9,19 @@ export default function Projects() {
     demo: "https://instagram-clone-pig9cvrvm-noclipping.vercel.app/",
     gif: "https://media2.giphy.com/media/CJl7OCbeJuZfIGSjuE/giphy.gif",
   };
+  const socialHumans = {
+    title: "Social-Humans",
+    background: "social_humans",
+    repo: "https://github.com/noclipping/social-media-app",
+    demo: "https://social-media-app-murex.vercel.app/",
+    gif: "https://media2.giphy.com/media/malhCP7UPRfsyC6Odo/giphy.gif?cid=790b7611b86d7c93b65e5f2aec2b5b9ef8dfb39c8e545f0d&rid=giphy.gif&ct=g",
+  };
   return (
     <section id="projects">
       <div className="cards_container">
         <Card project={instagram} />
 
-        <Card project={instagram} />
-
-        <Card project={instagram} />
+        <Card project={socialHumans} />
       </div>
     </section>
   );
