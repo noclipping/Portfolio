1b880b87c77eedf948c2a89b4585c9e360695b6e
aresous <aresous@gmail.com>
2022-07-21 22:29:02 -0400

    tnwclone project card

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import "../ProjectPage.css";
 import Card from "./Card";
 import socialHumansVid from "../images/socialhumansvid.mp4";
-
 import instacloneVid from "../images/instaclonevid.mp4";
+import tnwcloneVid from "../images/tnwclonevid.mp4";
 export default function Projects() {
   const instagram = {
     title: "InstaClone",
@@ -19,11 +19,19 @@ export default function Projects() {
     demo: "https://social-media-app-murex.vercel.app/",
     gif: socialHumansVid,
   };
+  const tnwClone = {
+    title: "TNW Clone",
+    background: "tnw_clone",
+    repo: "https://github.com/noclipping/TNWclone",
+    demo: "https://noclipping.github.io/TNWclone/",
+    gif: tnwcloneVid,
+  };
   return (
     <section id="projects">
       <div className="cards_container">
         <Card project={instagram} />
         <Card project={socialHumans} />
+        <Card project={tnwClone} />
       </div>
     </section>
   );
